d0b31f6a51edd4f046d47ebfa9f70749365646d3
Ritesh078bct <078bct068@student.ioepc.edu.np>
2024-12-17 17:31:57 +0545

    solved tree 1 by DFS

--- lab1/tree1.py
+++ lab1/tree1.py
@@ -1,11 +1,24 @@
 tree={
     'A':['B','C'],
-    'D':['G'],
     'B':['D','E'],
-    'G':[],
-    'E':['H'],
-    'H':[],
     'C':['F'],
-    'F':[]
+    'D':['G'],
+    'E':['H'],
+    'F':[],
+    'G':[],
+    'H':[],
 
 }
+
+stk=['A']
+visited=set()
+
+while stk:
+    node=stk.pop()
+    if node not in visited:
+        print(node,end=" ")
+        visited.add(node)
+
+        for neighbour in tree[node]:
+            if neighbour not in visited:
+                stk.append(neighbour)
