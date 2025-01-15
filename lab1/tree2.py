541856a76807c646b9b8098cf68b55bc87d848fe
Ritesh078bct <078bct068@student.ioepc.edu.np>
2025-01-15 09:17:23 +0545

    graph using dict

--- lab1/tree2.py
+++ lab1/tree2.py
@@ -1,5 +1,13 @@
-cities={
-    'Biratnagar':['Rangeli','Itahari','Biratchowk'],
-    'Rangeli':['Kanepokhari','Urlabari'],
-    ''
+graph = {
+    "Biratnagar": {"Itahari": 22, "Rangeli": 25},
+    "Itahari": {"Biratnagar": 22, "Dharan": 20, "Biratchowk": 30},
+    "Dharan": {"Itahari": 20},
+    "Biratchowk": {"Itahari": 30, "Kanepokhari": 10,"Biratnagar":30},
+    "Kanepokhari": {"Biratchowk": 10, "Urlabari": 12, "Rangeli": 25},
+    "Rangeli": {"Biratnagar": 25, "Kanepokhari": 25,"Urlabari":40},
+    "Urlabari": {"Kanepokhari": 12, "Damak": 6,"Rangeli":40},
+    "Damak": {"Urlabari": 6}
 }
+
+
+print("Graph :", graph)
